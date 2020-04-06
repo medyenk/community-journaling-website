6a375c48eba05c07ab6432451d9bb622e7262b5b
amita125 <amitaghale@gmail.com>
2020-04-06 13:38:29 +0100

    updated the file with the emoji

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express(); 
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 //organise the files PUBLIC + VIEWS if needed
-app.use(express.static('public'));
-app.use(express.static('views'));
-
+app.use(express.static("public"));
+app.use(express.static("views"));
 
 //CONVERT incoming JSON using bodyparser middleware to access POST handler from form
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 //MAIN ROUTE + Link page
-app.get('/', function(req, res){
-    console.log("hello world");
-    // res.sendFile(__dirname + '');
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "public/index.html");
+});
 
 // ROUTE - POST DATA to FORM
 //database - data POSTED from form will be parsed through middleware
-app.post('/', urlencodedParser, function(req, res){
-    console.log(req.body); 
-    res.render('success', {data: req.body});
+app.post("/", urlencodedParser, function (req, res) {
+  console.log(req.body);
+  res.render("success", { data: req.body });
 });
 
-
-app.listen(PORT, function() {
-    console.log(`Chirpin at ${PORT} `);
+app.listen(PORT, function () {
+  console.log(`Chirpin at ${PORT} `);
 });
