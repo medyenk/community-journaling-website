a9bf1c1eeea07ebdd7698b32e449ee95c86dd77f
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-04-07 12:13:43 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,23 +1,58 @@
-let fs = require("fs");
+$(document).ready(function () {
+  $("#show").click(function () {
+    $(".menu").toggle("slide");
+  });
+});
 
+//function to store DATA in file
 function storeData(req) {
-  let data = fs.readFileSync("posts.json");
-  data = JSON.parse(data);
+  data = getData("public\\posts.json");
   data.posts.push(req);
   let myJSON = JSON.stringify(data, null, 2);
-  fs.writeFileSync("posts.json", myJSON);
+  fs.writeFileSync("public\\posts.json", myJSON);
 }
 
+//function to get DATA from file
 function getData() {
-  let data = fs.readFileSync("posts.json");
+  let data = fs.readFileSync("public\\posts.json");
   data = JSON.parse(data);
   return data;
 }
 
-storeData({
-  text: "test",
-  emoji: 1,
-  date: "10:22 06/04/20",
-});
+fetch('./public/posts.json')
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        let result = `<h2> User Info From sampleUser.json </h2>`;
+        data.posts.forEach((user) => {
+            const { text, emoji, date } = user
+            console.log(data)
+            result +=
+            `<div class="old-post">
+                <figure>
+                  <img
+                    src="https://www.owlkids.com/wp-content/uploads/2017/02/chirp-pencil-for-slug-e1487097360834-1024x910.jpg"
+                    alt="chirp bird"
+                  />
+                </figure>
+                <div class="post-view">
+                  <p>${text}</p>
+                </div>
+                <div id="show"><i class="far fa-comments"> Comment</i></div>
+                <div class="menu">
+                  <form class="comment_form" method="POST">
+                    <textarea name="comment_content"></textarea>
+                    <input
+                      type="submit"
+                      name="postComment"
+                      value="Post"
+                      id="commentButton"
+                    />
+                  </form>
+                </div>
+              </div>`;
 
-console.log(getData());
+            document.getElementById('posts_area').innerHTML = result;
+        });
+       })
